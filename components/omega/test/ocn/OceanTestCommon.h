3d11a4168903474f9d5d1e31baca8af2edc91706
Kat Smith <kat.smith133@gmail.com>
2025-06-12 09:33:28 -0700

    moves logging and adds a function for GPU kernel considerations

--- components/omega/test/ocn/OceanTestCommon.h
+++ components/omega/test/ocn/OceanTestCommon.h
@@ -10,10 +10,14 @@
 
 namespace OMEGA {
 
-// check if two real numbers are equal with a given relative tolerance
-inline bool isApprox(Real X, Real Y, Real RTol, Real ATol = 0) {
-   return std::abs(X - Y) <=
-          std::max(ATol, RTol * std::max(std::abs(X), std::abs(Y)));
+// check if two real numbers are equal with a given relative tolerance (CPU)
+inline bool isApprox(Real X, Real Y, Real RTol) {
+   return std::abs(X - Y) <= RTol * std::max(std::abs(X), std::abs(Y));
+}
+
+// check if two real numbers are equal with a given relative tolerance (GPU)
+KOKKOS_INLINE_FUNCTION bool isApproxGPU(Real X, Real Y, Real RTol) {
+   return (X > Y ? X - Y : Y - X) <= RTol * (X > Y ? X : Y);
 }
 
 // convert spherical components of a vector to Cartesian
